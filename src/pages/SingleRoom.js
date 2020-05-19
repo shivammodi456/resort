08bcaacd632e95d504e42b861c2d991ec41b5b15
Shivam Modi <shivammodi456@gmail.com>
2020-05-19 11:21:07 +0530

    Navbar Done

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello From SingleRoom.js</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleRoom;
