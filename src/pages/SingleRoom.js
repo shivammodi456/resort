8a4e96700bba99253caa2c0e9e54d410be976e64
Shivam Modi <shivammodi456@gmail.com>
2020-05-20 15:06:14 +0530

    SinglePage HeroBanner - Styled Component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import { RoomContext } from "../context";
+import { Link } from "react-router-dom";
+import Hero from "../component/Hero";
+import Banner from "../component/Banner";
+import StyledHero from "../component/StyledHero";
 
 export class SingleRoom extends Component {
+  state = {
+    slug: this.props.match.params.slug,
+    defaultBcg,
+  };
+
+  static contextType = RoomContext;
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    // console.log(room);
+
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No Such Room Could be found..</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+
+    const {
+      name,
+      descroption,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+
     return (
-      <div>
-        <h1>Hello From SingleRoom.js</h1>
-      </div>
+      <>
+        <StyledHero img={images[0] || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to room
+            </Link>
+          </Banner>
+        </StyledHero>
+      </>
     );
   }
 }
